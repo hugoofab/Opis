275b7094c103e2fbdd1be939e380d4dc0d170f83
Hugo F. A. Ferreira <hugoofab@gmail.com>
2017-02-14 08:40:25 -0300

    adicionado envio de headers adequados para uma saída json

--- Response.php
+++ Response.php
@@ -30,6 +30,14 @@ class Opis_Response {
 	public function __toString ( ) {
 		switch ($this->outputType) {
 			case 'json':
+				// para evitar problemas de encoding, pode-se fazer um utf8_encode ma mensagem ou dados da saida
+				header("Content-Type:application/json; charset=utf-8");
+
+				if ( isset($_SERVER['HTTP_ACCEPT_ENCODING']) && substr_count($_SERVER['HTTP_ACCEPT_ENCODING'], 'gzip') ) {
+				    ini_set('zlib.output_compression', 'on');
+				    header('Content-Encoding:gzip');
+				}
+				
 				return json_encode( $this->response );
 			break;
 			default:
